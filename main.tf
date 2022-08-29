bc949bdce295f60507fb492e813c1bfff03a24fe
amikshas <46197570+amikshas@users.noreply.github.com>
2022-08-29 19:02:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region                  = "us-east-1"
   access_key              = var.access_key
-  secret_key              = var.secret_key
-  token                   = var.session_token
+  secret_key              = var.secret_key  
 }
 
 resource "tls_private_key" "private-key" {
@@ -11,7 +10,7 @@ resource "tls_private_key" "private-key" {
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
+  key_name   = "deployer-key-win"
   public_key = tls_private_key.private-key.public_key_openssh
 }
 
@@ -31,18 +30,18 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_security_group" "student-sg" {
-  name = "Hello-World-SG"
+resource "aws_security_group" "student-sg-win" {
+  name = "Hello-World-SG-win"
   description = "Student security group"
 
   tags = {
-    Name = "Hello-World-SG"
+    Name = "Hello-World-SG-win"
     Environment = terraform.workspace
   }
 }
 
 resource "aws_security_group_rule" "create-sgr-ssh" {
-  security_group_id = aws_security_group.student-sg.id
+  security_group_id = aws_security_group.student-sg-win.id
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 22
   protocol          = "tcp"
@@ -51,7 +50,7 @@ resource "aws_security_group_rule" "create-sgr-ssh" {
 }
 
 resource "aws_security_group_rule" "create-sgr-inbound" {
-  security_group_id = aws_security_group.student-sg.id
+  security_group_id = aws_security_group.student-sg-win.id
   cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 0
   protocol          = "all"
@@ -60,7 +59,7 @@ resource "aws_security_group_rule" "create-sgr-inbound" {
 }
 
 resource "aws_security_group_rule" "create-sgr-outbound" {
-  security_group_id = aws_security_group.student-sg.id
+  security_group_id = aws_security_group.student-sg-win.id
   cidr_blocks         = ["0.0.0.0/0"]
   from_port         = 0
   protocol          = "all"
@@ -68,28 +67,28 @@ resource "aws_security_group_rule" "create-sgr-outbound" {
   type              = "egress"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "webwin" {
   count         = 2 
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
-  security_groups = ["Hello-World-SG"]
+  security_groups = ["Hello-World-SG-win"]
   tags = {
-    Name = "Web${count.index}"
+    Name = "Webwin${count.index}"
   }
 }
 
 resource "null_resource" "control-node" {
-    depends_on = [aws_instance.web]
+    depends_on = [aws_instance.webwin]
   
     connection {
       type        = "ssh"
       user        = "ubuntu"
       private_key = tls_private_key.private-key.private_key_pem
-      host        = aws_instance.web.*.public_dns[0]
+      host        = aws_instance.webwin.*.public_dns[0]
     }
     provisioner "local-exec" {
-      command = "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/student.pem && chmod 600 ~/.ssh/student.pem "
+      command = "echo '${tls_private_key.private-key.private_key_pem}' > ~/Desktop/studentwin.pem && chmod 600 ~/Desktop/studentwin.pem "
     }
     provisioner "remote-exec" {
       inline = [
@@ -97,8 +96,8 @@ resource "null_resource" "control-node" {
 	"sudo apt install python3-pip -y",
         "sudo apt install ansible -y",
         "echo '[ciservers]' > ~/hosts",
-        "echo '${aws_instance.web.*.public_dns[1]}' >> ~/hosts",
-        "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/student.pem && chmod 600 ~/.ssh/student.pem",
+        "echo '${aws_instance.webwin.*.public_dns[1]}' >> ~/hosts",
+        "echo '${tls_private_key.private-key.private_key_pem}' > ~/.ssh/studentwin.pem && chmod 600 ~/.ssh/studentwin.pem",
         "sudo sed -i '71s/.*/host_key_checking = False/' /etc/ansible/ansible.cfg"        
       ]
     }
