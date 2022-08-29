d2df50efc17bdba5f2300dc946299b83496082cf
amikshas <46197570+amikshas@users.noreply.github.com>
2022-08-29 19:03:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,12 @@
 variable "access_key" {
 	description = "access_key for aws"
 	type        = string
-	default     = "ASIA6D4UPZ55WE2MBF7X"
+	default     = "<access_key>"
 }
 
 variable "secret_key" {
 	description = "secret_key for aws"
 	type        = string
-	default     = "lia+YW/1RmZ5BnZoCfVMABuuZKABWq+6E8UqhFDQ"
+	default     = "<secret_key>"
 }
 
-variable "session_token" {
-	description = "session_token for aws"
-	type        = string
-	default     = "FwoGZXIvYXdzEJb//////////wEaDOjvOczqzVqF95ly/CK6Ae5PjK6FTPXka0HOGUnk9KKOgj6lUikwPcwI8AZaFe89KmLmcf010LB0bWBjRqabbu5GhYDQuSOOm3E0P02dW5yLHCLZjuEzR39nE6+OeILOxUFaqS4BiUEfYy3kGtULG06cOcIp7Gfd9u+FP8YgwptClsLZ2ss1btVFZ52pwLbGTrU8PX091F7ZKcp+/NiVdDH8+JMAvQ/IKFGafDhFBmvHCXW8R6KiHi27Gg/R0MdNQvDrndBu6GWkAiio6KuYBjItNMjAa8yObGc1zJuYgo7QbwaKESkQxef7/DT7o9mXXm0sSshesqwckp7oUai0"
-}
